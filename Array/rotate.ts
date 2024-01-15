c70cb981ad833d178b3b67d29f08d0bb86073433
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-15 14:54:18 +0530

    DSA Typescript

--- Array/rotate.ts
+++ Array/rotate.ts
@@ -12,7 +12,7 @@ function reverseInrange(nums:number[],l:number,r:number){
 
 var rotate = function (nums:number[],k:number){
     let n = nums.length;
-    k = k% n
+    k = k%n
     reverseInrange(nums,n-k,n-1)
     reverseInrange(nums,0,n-k-1)
     reverseInrange(nums,0,n-1)
