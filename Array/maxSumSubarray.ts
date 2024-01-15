ae317155d904424d16c7e53e773bbfc0506f2248
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-15 10:37:32 +0530

    DSA Typescript

---
+++ Array/maxSumSubarray.ts
@@ -0,0 +1,14 @@
+function maxSumSubarray(arr:number[]):number{
+    let train : number =  0;
+    let maxi : number = 0;
+    for(let i = 0;i<arr.length;i++){
+        let newTrain = arr[i]
+        let prevTrain = train + arr[i]
+        train = Math.max(newTrain,prevTrain)
+        maxi = Math.max(maxi,train)
+    }
+    return maxi;
+
+}
+let ari = [5,-2,-7,3,-4,-2,6,7,-2,8,-1,10,-2,-4,2]
+console.log(maxSumSubarray(ari))
