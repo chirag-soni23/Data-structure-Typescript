726988741fc3687929711e2c2c3bf3c705de548d
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-16 08:05:50 +0530

    DSA Typescript

---
+++ 2d-Array/intro.js
@@ -0,0 +1,8 @@
+"use strict";
+let aarr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+console.log(aarr.length);
+console.log(aarr[1][2]);
---
+++ 2d-Array/intro.ts
@@ -0,0 +1,7 @@
+let aarr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+console.log(aarr.length);
+console.log(aarr[1][2]);
