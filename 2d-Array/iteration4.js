141f8c404c6b4b96cfb6243efdc2d317b0df1ad3
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-16 11:17:51 +0530

    DSA Typescript

---
+++ 2d-Array/iteration4.js
@@ -0,0 +1,25 @@
+"use strict";
+let arr5 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+function iteration4(arr) {
+    let s = "";
+    for (let i = 0; i < arr.length; i++) {
+        if (i % 2 == 0) {
+            // even row left to right
+            for (let j = 0; j < arr.length; j++) {
+                s += arr[i][j] + " ";
+            }
+        }
+        else {
+            // odd row right to left
+            for (let j = arr[i].length - 1; j >= 0; j--) {
+                s += arr[i][j] + " ";
+            }
+        }
+    }
+    console.log(s);
+}
+iteration4(arr5);
