17b39316c41612f8b0c7f63f8d99ca7c9b595f45
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-16 11:01:47 +0530

    DSA Typescript

---
+++ 2d-Array/rotate.ts
@@ -0,0 +1,33 @@
+let arr4 = [
+  [1, 2, 3],
+  [4, 5, 6],   // => 90 degree rotate
+  [7, 8, 9],
+];
+function reversearr(arr:number[]){
+    let l = 0
+    let r = arr.length -1
+    while(l<r){
+        let temp = arr[l]
+        arr[l] = arr[r]
+        arr[r] = temp
+        l++;
+        r--
+    }
+    for(let i = 0;i<arr3.length;i++){
+        reversearr(arr4[i])
+    }
+}
+
+function rotatearr(arr:number[][]){
+    for(let i = 0;i<arr.length;i++){
+        // console.log(i)
+        for(let j =i ;j<arr[0].length;j++){
+            let temp = arr[i][j]
+            arr[i][j] = arr[j][i]
+            arr[j][i] = temp
+        }
+
+    }
+console.log(arr)
+}
+rotatearr(arr4)
