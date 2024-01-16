e37ffbb4fa781cfcebe2ff3c97297ad7cc7972c3
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-16 08:28:02 +0530

    DSA Typescript

--- 2d-Array/intro.js
+++ 2d-Array/intro.js
@@ -4,5 +4,16 @@ let aarr = [
     [4, 5, 6],
     [7, 8, 9],
 ];
-console.log(aarr.length);
-console.log(aarr[1][2]);
+// console.log(aarr.length);
+// console.log(aarr[1][2]);
+function iteration1(arr) {
+    let s = "";
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            console.log(arr[i].length);
+            s += arr[i][j] + " ";
+        }
+    }
+    console.log(s);
+}
+iteration1(aarr);
--- 2d-Array/intro.ts
+++ 2d-Array/intro.ts
@@ -3,5 +3,16 @@ let aarr = [
   [4, 5, 6],
   [7, 8, 9],
 ];
-console.log(aarr.length);
-console.log(aarr[1][2]);
+// console.log(aarr.length);
+// console.log(aarr[1][2]);
+
+function iteration1(arr: number[][]) {
+  let s: string = "";
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      s += arr[i][j] + " ";
+    }
+  }
+  console.log(s);
+}
+iteration1(aarr);
