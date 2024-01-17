8832ae052294d373eaf7a934fc9b9d6e4f23d2c3
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-17 18:03:56 +0530

    DSA Typescript

---
+++ Strings/subString.js
@@ -0,0 +1,10 @@
+"use strict";
+function subString(s) {
+    for (let i = 0; i < s.length; i++) {
+        for (let j = i; j < s.length; j++) {
+            // We have to print the substring from i to j
+            console.log(s.substring(i, j + 1));
+        }
+    }
+}
+subString("abcd");
