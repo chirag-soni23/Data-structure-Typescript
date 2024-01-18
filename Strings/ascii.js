0cfef97931ba9304ba0197177839fcbb0719bafb
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-18 08:03:52 +0530

    DSA Typescript

--- Strings/ascii.js
+++ Strings/ascii.js
@@ -1,3 +1,23 @@
 "use strict";
-console.log(String.fromCharCode(65));
-console.log(String.fromCharCode(47));
+// console.log(String.fromCharCode(65))
+// console.log(String.fromCharCode(47))
+// console.log('ca'.charCodeAt(1))
+// Toogle ascii value
+function toggleCase(s) {
+    let ans = "";
+    for (let i = 0; i < s.length; i++) {
+        let ascii = s.charCodeAt(i);
+        if (ascii < 97) {
+            // I am a capital letter,convert me to small letter by adding 32
+            ascii += 32;
+        }
+        // I am a small letter,convert me to small letter by minus 32
+        else {
+            ascii -= 32;
+        }
+        ans += String.fromCharCode(ascii);
+    }
+    return ans;
+}
+let asciiresult = toggleCase("abCdeFgh");
+console.log(asciiresult);
--- Strings/ascii.ts
+++ Strings/ascii.ts
@@ -1,2 +1,25 @@
-console.log(String.fromCharCode(65))
-console.log(String.fromCharCode(47))
+// console.log(String.fromCharCode(65))
+// console.log(String.fromCharCode(47))
+// console.log('ca'.charCodeAt(1))
+
+// Toogle ascii value
+function toggleCase(s:string):string{
+    let ans:string = ""
+    for(let i = 0;i<s.length;i++){
+        let ascii = s.charCodeAt(i)
+        if(ascii <97){
+            // I am a capital letter,convert me to small letter by adding 32
+            ascii += 32
+        }
+            // I am a small letter,convert me to capital letter by minus 32
+            else{
+                ascii -= 32
+            }
+            ans+= String.fromCharCode(ascii)
+
+    }
+    return ans;
+
+}
+let asciiresult = toggleCase("abCdeFgh")
+console.log(asciiresult)
