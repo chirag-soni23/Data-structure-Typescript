7d3af5c2516e006f03d69d7a64b8ea4d8c5917db
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-18 14:02:01 +0530

    DSA Typescript

---
+++ Strings/prefix.ts
@@ -0,0 +1,14 @@
+function longestCommonPreffix(arr:string[]):string{
+    let ans:string = ""
+    for(let i =0 ;i<arr[0].length;i++){
+        for(let j = 1;j<arr.length;j++){
+            if(i>=arr[j].length || arr[0][i] != arr[j][i]){
+                return ans
+            }
+        }
+        ans += arr[0][i]
+
+    }
+    return ans
+}
+console.log(longestCommonPreffix(["flower","flow","flight"]))
