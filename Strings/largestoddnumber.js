1f442db6a8c2aa00a5e9365abf31a453b8124069
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-19 08:25:39 +0530

    DSA Typescript

---
+++ Strings/largestoddnumber.js
@@ -0,0 +1,12 @@
+"use strict";
+function largestOddNumber(s) {
+    let i = s.length - 1;
+    while (i >= 0) {
+        if (parseInt(s[i]) % 2 == 1) {
+            break;
+        }
+        i--;
+    }
+    return s.substring(0, i + 1);
+}
+console.log(largestOddNumber("52"));
