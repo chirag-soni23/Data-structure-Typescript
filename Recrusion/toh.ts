777298ec306a109ea91a0a7d8a4eab423bb846ad
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-21 08:32:59 +0530

    DSA Typescript

--- Recrusion/toh.ts
+++ Recrusion/toh.ts
@@ -1,9 +1,8 @@
 // toh => Tower of hanoi
-function toh(n:number,s:string,d:string,h:string){
-    if(n==0)return;
-    toh(n-1,s,h,d)
-    console.log(`Move disc ${n} from ${s} to ${d}`)
-    toh(n-1,h,d,s)
-
+function toh(n: number, s: string, d: string, h: string) {
+  if (n == 0) return;
+  toh(n - 1, s, h, d);
+  console.log(`Move disc ${n} from ${s} to ${d}`);
+  toh(n - 1, h, d, s);
 }
-toh(3,"a","b","c")
+toh(3, "a", "b", "c");
