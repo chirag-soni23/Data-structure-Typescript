2f4b10eb2ab86748f712388c07e75db56ada12f3
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-21 19:48:22 +0530

    DSA Typescript

--- 2d-Array/iteration3.js
+++ 2d-Array/iteration3.js
@@ -8,7 +8,6 @@ function iteration3(arr) {
     let s = "";
     for (let j = 0; j < arr[0].length; j++) {
         for (let i = arr.length - 1; i >= 0; i--) {
-            console.log(i);
             s += arr[i][j] + " ";
         }
     }
