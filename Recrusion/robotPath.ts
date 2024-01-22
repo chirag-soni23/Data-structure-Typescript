268ea2245b31a6c2d0b49184853668d69829e8ed
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-22 10:02:59 +0530

    DSA Typescript

--- Recrusion/robotPath.ts
+++ Recrusion/robotPath.ts
@@ -1,13 +1,13 @@
 // psf => path so far
-function robotPath(n:number,m:number,i:number,j:number,psf:string){
-    if(i==n&& j==n){
-        console.log(psf)
-        return
-    }
-    if(i>n||j>n) return;
-    // Vertically
-    robotPath(n,m,i+1,j,psf+"v")
-    // Horizontally
-    robotPath(n,m,i,j+1,psf+"h")
+function robotPath(n: number, m: number, i: number, j: number, psf: string) {
+  if (i == n && j == n) {
+    console.log(psf);
+    return;
+  }
+  if (i > n || j > n) return;
+  // Vertically
+  robotPath(n, m, i + 1, j, psf + "v");
+  // Horizontally
+  robotPath(n, m, i, j + 1, psf + "h");
 }
-robotPath(3,3,1,1,"")
+robotPath(3, 3, 1, 1, "");
