0fa69444b0e088c6a78a3648ad5678f26cb71c59
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-22 10:04:21 +0530

    DSA Typescript

--- Recrusion/factorial.ts
+++ Recrusion/factorial.ts
@@ -1,6 +1,6 @@
-function factorial(n:number):number{
-    if(n==1)return 1
-    let nm1f = factorial(n-1)
-    return n *  nm1f
+function factorial(n: number): number {
+  if (n == 1) return 1;
+  let nm1f = factorial(n - 1);
+  return n * nm1f;
 }
-console.log(factorial(5))
+console.log(factorial(5));
