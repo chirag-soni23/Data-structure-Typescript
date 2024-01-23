7b22c17eb4f5eeffefc58ebfa14c9ca9052fe692
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-23 10:58:18 +0530

    DSA Typescript

--- Recrusion/countsairsPath.ts
+++ Recrusion/countsairsPath.ts
@@ -2,7 +2,7 @@ function countStairspath(n: number, currStair: number): number {
   // Base Case
   if (currStair == n) return 1;
   if (currStair > n) return 0;
-  // recrusve function
+  // recrusive function
   let one = countStairspath(n, currStair + 1);
   let two = countStairspath(n, currStair + 2);
   let three = countStairspath(n, currStair + 3);
