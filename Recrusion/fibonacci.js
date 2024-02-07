a94caef1199ff22bb766f8673e3dd980e77fa580
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-07 11:47:01 +0530

    DSA Typescript

--- Recrusion/fibonacci.js
+++ Recrusion/fibonacci.js
@@ -1,16 +1,14 @@
 "use strict";
 function fibonacci(n) {
-    // Base Case
-    if (n == 1)
-        return 0;
-    if (n == 2)
-        return 1;
-    // Not Base Case
-    // to calculate nth fibonacci number i need n-1th and n-2th fibonacci number
-    let nm1 = fibonacci(n - 1);
-    let nm2 = fibonacci(n - 2);
-    // I have both ingredients for my dish 
-    // dish = nth fibonacci number
-    return nm1 + nm2;
+  // Base Case
+  if (n == 1) return 0;
+  if (n == 2) return 1;
+  // Not Base Case
+  // to calculate nth fibonacci number i need n-1th and n-2th fibonacci number
+  let nm1 = fibonacci(n - 1);
+  let nm2 = fibonacci(n - 2);
+  // I have both ingredients for my dish
+  // dish = nth fibonacci number
+  return nm1 + nm2;
 }
 console.log(fibonacci(8));
