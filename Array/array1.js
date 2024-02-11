6ae543df7b91ff8eff8acfc441c1c19deb5757f4
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-11 12:41:53 +0530

    DSA Typescript

--- Array/array1.js
+++ Array/array1.js
@@ -1,29 +1,26 @@
 "use strict";
 function getmax(arr) {
-    let maxi = -Infinity;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > maxi)
-            maxi = arr[i];
-    }
-    return maxi;
+  let maxi = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > maxi) maxi = arr[i];
+  }
+  return maxi;
 }
 let arr = [5, 4, 2, 1, 6, 7];
 // let result = getmax([1,2,5,6,7,8,9])
 // console.log(result)
 function printBarChart(arr) {
-    let cols = arr.length;
-    let rows = getmax(arr);
-    while (rows > 0) {
-        let s = "";
-        for (let j = 0; j < cols; j++) {
-            if (arr[j] >= rows)
-                s += "*";
-            else
-                s += " ";
-        }
-        console.log(s);
-        // console.log(rows--)
-        rows--;
+  let cols = arr.length;
+  let rows = getmax(arr);
+  while (rows > 0) {
+    let s = "";
+    for (let j = 0; j < cols; j++) {
+      if (arr[j] >= rows) s += "*";
+      else s += " ";
     }
+    console.log(s);
+    // console.log(rows--)
+    rows--;
+  }
 }
 printBarChart(arr);
