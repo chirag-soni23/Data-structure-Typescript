7b4c66aab06eff8c6c1c9968ec975bdc670a5099
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-11 12:43:06 +0530

    DSA Typescript

--- Array/floor.js
+++ Array/floor.js
@@ -1,22 +1,20 @@
 "use strict";
 function getFloor(arr, target) {
-    let l = 0;
-    let r = arr.length - 1;
-    let floor = -1;
-    while (l <= r) {
-        let mid = Math.floor((l + r) / 2);
-        if (arr[mid] == target) {
-            return target;
-        }
-        else if (arr[mid] < target) {
-            floor = arr[mid];
-            l = mid + 1;
-        }
-        else {
-            r = mid - 1;
-        }
+  let l = 0;
+  let r = arr.length - 1;
+  let floor = -1;
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2);
+    if (arr[mid] == target) {
+      return target;
+    } else if (arr[mid] < target) {
+      floor = arr[mid];
+      l = mid + 1;
+    } else {
+      r = mid - 1;
     }
-    return floor;
+  }
+  return floor;
 }
 let a = [10, 12, 23, 34, 39, 40, 45, 52, 59, 63, 70, 90, 101];
 let value = getFloor(a, 80);
